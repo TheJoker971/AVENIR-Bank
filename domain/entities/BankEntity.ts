e03ff67119e3759c7543f31603cb56e799940e15
Joackim SEGOR <81244842+TheJoker971@users.noreply.github.com>
2025-10-09 09:46:59 +0200

    Domain (#3)
    * UserEntity and validations
    
    * Restucture UserEntity
    
    * Starting Account
    
    * AccountEntity
    
    * Fix import issue AccountNumber
    
    * Adding method in Entitied
    
    * Adding BankEntity
    
    * Fix relations between BankEntity and AccountEntity
    
    * Adding instructions
    
    * Delete Taux
    
    * Adding readonly and remove function in entities
    
    * Adding readonly and remove function in entities
    
    * Update all Entities
    
    * Adding interestRate in BankEntity (#1)

--- domain/entities/BankEntity.ts
+++ domain/entities/BankEntity.ts
@@ -1,6 +1,8 @@
 import { BankCode } from "domain/values/BankCode";
 import { BranchCode } from "domain/values/BranchCode";
 import { UserEntity } from "./UserEntity";
+import { InterestRate } from "domain/values/InterestRate";
+
 
 export class BankEntity {
 
@@ -9,6 +11,9 @@ export class BankEntity {
         public readonly name:string,
         public readonly bankCode:BankCode,
         public readonly branche: BranchCode,
+
+        public readonly interestRate : InterestRate,
+
     ){
     }
 
@@ -17,7 +22,9 @@ export class BankEntity {
         if(bankCodeOrError instanceof Error) return bankCodeOrError;
         const branchOrError = BranchCode.create(branch);
         if(branchOrError instanceof Error) return branchOrError;
-        return new BankEntity(name, bankCodeOrError, branchOrError);
+        const interestRateOrError = InterestRate.create(0);
+        if(interestRateOrError instanceof Error) return interestRateOrError;
+        return new BankEntity(name, bankCodeOrError, branchOrError, interestRateOrError);
     }
 
 }
