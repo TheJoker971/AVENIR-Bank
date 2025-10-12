d39d46c42942a4456612a4db7ce27cc2917161f9
TheJoker971 <jojo.blsegor@gmail.com>
2025-10-12 21:12:45 +0200

    Creating MessageEntity

--- domain/entities/MessageEntity.ts
+++ domain/entities/MessageEntity.ts
@@ -5,13 +5,12 @@ export class MessageEntity {
         public readonly id:number,
         public readonly senderId:number,
         public readonly receiverId:number,
-        public readonly subject:string,
         public readonly message:string,
         public readonly date:Date=new Date(),
         ){
     }
 
-    public static create(id: number,senderId:number,receiverId:number,subject:string,message:string):MessageEntity{
-        return new MessageEntity(id,senderId,receiverId,subject,message);
+    public static create(id: number,senderId:number,receiverId:number,message:string):MessageEntity{
+        return new MessageEntity(id,senderId,receiverId,message);
     }
 }
