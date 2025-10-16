dc6dba029b99806c087f51d072012943afe92180
Joackim SEGOR <81244842+TheJoker971@users.noreply.github.com>
2025-10-16 10:35:39 +0200

    rebase (#9)
    * Create bank repository (#6)
    
    * Create BankRepositoryInterface
    
    * Adding BankRepositoryInterface
    
    * Creating MessageEntity (#7)
    
    * Starting OperationEntity (#8)

--- application/repositories/BankRepositoryInterface.ts
+++ application/repositories/BankRepositoryInterface.ts
@@ -3,8 +3,9 @@ import { BankEntity } from "domain/entities/BankEntity";
 export interface BankRepositoryInterface {
   getCurrentBank(): Promise<BankEntity | null>;
   findById(id: number): Promise<BankEntity | null>;
-  save(bank: BankEntity): Promise<void>;
-  update(bank: BankEntity): Promise<void>;
+  save(bank: BankEntity): Promise<void | Error>;
+  update(bank: BankEntity): Promise<void | Error>;
   delete(id: number): Promise<void>;
   exists(id: number): Promise<boolean>;
 }
+
